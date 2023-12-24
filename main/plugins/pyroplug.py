de704e197ae07390344594856be12cdde7dfbbf8
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2023-12-24 14:42:33 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -32,7 +32,7 @@ async def get_msg(userbot, client, bot, sender, edit_id, msg_link, i):
     if "?single" in msg_link:
         msg_link = msg_link.split("?single")[0]
     msg_id = int(msg_link.split("/")[-1]) + int(i)
-    height, width, duration, thumb_path = 90, 90, 0, None
+    height, width, duration, thumb_path = msg.video.height, msg.video.width, msg.video.duration, acc.download_media(msg.video.thumbs[0].file_id)
     if 't.me/c/' or 't.me/b/' in msg_link:
         if 't.me/b/' in msg_link:
             chat = str(msg_link.split("/")[-2])
